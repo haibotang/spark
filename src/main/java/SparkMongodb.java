d05a399d553a50d3aa5c43d4c54861becd26c68e
‘唐海波 <lizhoutang/112>
2019-03-22 15:41:51 +0800

    mongodb-demo

--- src/main/java/SparkMongodb.java
+++ src/main/java/SparkMongodb.java
@@ -1,28 +1,41 @@
 import com.mongodb.spark.MongoSpark;
 import model.Book;
+import model.BookDesc;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.Properties;
+
 public class SparkMongodb {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
+//        String url = "mongodb://127.0.0.1:27017/test";
 //        SparkSession session = SparkSession.builder().master("local").appName("MongoSparkConnectorIntro")
 //                .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/test")
+//                .config("spark.mongodb.input.collection","t_book")
 //                .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/test")
 //                .getOrCreate();
-//        MongoSpark.load(session);
+//        MongoSpark.load(session,Book.class);
+//        MongoSpark.load(session, BookDesc.class);
+//        Dataset dataset = session.sql("select a.id,count(1) from t_book a join t_book_desc b on b.b_id = a.id group
+// by a.id");
+//        dataset.show();
 
-        SparkConf conf = new SparkConf().setAppName("mongo").setMaster("local").set("spark.mongodb.input.uri","mongodb://127.0.0.1:27017/test.t_book");
+        SparkConf conf = new SparkConf().setAppName("mongo").setMaster("local").set("spark.mongodb.input.uri",
+                "mongodb://127.0.0.1:27017/test.t_book").set("spark.mongodb.input.uri",
+                "mongodb://127.0.0.1:27017/test.t_book_desc");
         JavaSparkContext jsc = new JavaSparkContext(conf);
 
         SQLContext sqlContext = SQLContext.getOrCreate(jsc.sc());
-        MongoSpark.load(jsc).toDF().createOrReplaceTempView("t_book");
-        MongoSpark.load(jsc).toDF().createOrReplaceTempView("t_book_desc");
-//        Dataset dataset = sqlContext.sql("select * from t_book");
-        Dataset dataset = sqlContext.sql("select a.id,count(1) from t_book a join t_book_desc b on b.b_id = a.id group by a.id");
+        Dataset<Row> df = MongoSpark.load(jsc).toDF();
+        df.createOrReplaceTempView("t_book");
+        df.createOrReplaceTempView("t_book_desc");
+        Dataset dataset = sqlContext.sql("select a.id,count(1) from t_book a join t_book_desc b on b.b_id = a.id " +
+                "group by a.id");
         //显示数据
         dataset.show();
     }
